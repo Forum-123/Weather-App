715dda6bec0abb6cb97168a4c4782d071b20e7c9
Forum-123 <69541616+Forum-123@users.noreply.github.com>
2021-11-24 12:22:44 +0000

    Serve content over HTTPS

--- request.js
+++ request.js
@@ -1,18 +1,20 @@
 // Accessing the Weather API
 const key = '762aa9c00ad9320d5e74709fb42178de';
 
-const currentWeather = 'http://api.openweathermap.org/data/2.5/weather?q=London&appid=762aa9c00ad9320d5e74709fb42178de';
+const currentWeather = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=762aa9c00ad9320d5e74709fb42178de';
 
 fetch(currentWeather)
-    .then((weatherData) => { console.log('response', weatherData.json()) })
-    .catch((error)=>{console.log(error)});
+    .then((weatherData) => {
+        console.log('response', weatherData.json())
+    })
+    .catch((error)=>{
+        console.log(error)
+    });
 
 const requestCity = async (city) => {
     const currentWeather = 'https://api.openweathermap.org/data/2.5/weather'
     const query = '?q=' + city + '&appid=' + key;
-
     const response = await fetch(currentWeather + query);
-
     const data = await response.json();
     return data;
 }
